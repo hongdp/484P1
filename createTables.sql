f0ff770734d30b6902c644774691b3f90678fb78
Dapeng Hong <hongdp@gmail.com>
2015-09-30 14:47:55 -0400

    cm

--- createTables.sql
+++ createTables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Location(
-	location_ID INTEGER,
+	location_ID INTEGER NOT NULL,
 	city VARCHAR2(100),
 	state VARCHAR2(100),
 	country VARCHAR2(100),
@@ -7,12 +7,12 @@ CREATE TABLE Location(
 );
 
 CREATE TABLE FB_User(
-	user_ID VARCHAR2(100),
-	first_name VARCHAR2(100),
-	last_name VARCHAR2(100),
-	year_of_birth INTEGER,
-	month_of_birth INTEGER,
-	date_of_birth INTEGER,
+	user_ID VARCHAR2(100) NOT NULL,
+	first_name VARCHAR2(100) NOT NULL,
+	last_name VARCHAR2(100) NOT NULL,
+	year_of_birth NUMBER(38),
+	month_of_birth NUMBER(38),
+	date_of_birth NUMBER(38),
 	gender VARCHAR2(100),
 	home_town_ID INTEGER,
 	current_location_ID INTEGER,
@@ -22,17 +22,17 @@ CREATE TABLE FB_User(
 );
 
 CREATE TABLE Event(
-	Event_ID VARCHAR2(100),
-	Event_creator_ID VARCHAR2(100),
+	Event_ID VARCHAR2(100) NOT NULL,
+	Event_creator_ID VARCHAR2(100) NOT NULL,
 	Event_location_ID INTEGER,
-	Event_host VARCHAR2(100),
-	Event_name VARCHAR2(100),
+	Event_host VARCHAR2(100) NOT NULL,
+	Event_name VARCHAR2(100) NOT NULL,
 	Event_tagline VARCHAR2(1000),
 	Event_description VARCHAR2(4000),
-	Event_type VARCHAR2(100),
-	Event_subtype VARCHAR2(100),
-	Event_start_time TIMESTAMP(6),
-	Event_end_time TIMESTAMP(6),
+	Event_type VARCHAR2(100) NOT NULL,
+	Event_subtype VARCHAR2(100) NOT NULL,
+	Event_start_time TIMESTAMP(6) NOT NULL,
+	Event_end_time TIMESTAMP(6) NOT NULL,
 	Event_location VARCHAR2(100),
     PRIMARY KEY (Event_ID),
     FOREIGN KEY (Event_creator_ID) REFERENCES FB_User,
@@ -40,8 +40,8 @@ CREATE TABLE Event(
 );
 
 CREATE TABLE Participate(
-	Event_ID VARCHAR2(100),
-	user_ID VARCHAR2(100),
+	Event_ID VARCHAR2(100) NOT NULL,
+	user_ID VARCHAR2(100) NOT NULL,
 	confirmation VARCHAR2(100),
 	PRIMARY KEY (Event_ID, user_ID),
 	FOREIGN KEY (Event_ID) REFERENCES Event,
@@ -49,16 +49,17 @@ CREATE TABLE Participate(
 );
 
 CREATE TABLE Education_program(
-	program_ID INTEGER,
+	program_ID INTEGER NOT NULL,
 	institution_name VARCHAR2(100),
-	concentration VARCHAR2(100),
+	concentration CHAR(100),
 	degree VARCHAR2(100),
 	PRIMARY KEY (program_ID)
 );
 
 CREATE TABLE Attended_program(
-	user_ID VARCHAR2(100),
-	program_ID INTEGER,
+	user_ID VARCHAR2(100) NOT NULL,
+	program_ID INTEGER NOT NULL,
+	year_of_graduation NUMBER(38),
 	PRIMARY KEY (user_ID, program_ID),
 	FOREIGN KEY (user_ID) REFERENCES FB_User,
 	FOREIGN KEY (program_ID) REFERENCES Education_program
@@ -84,37 +85,36 @@ CREATE TABLE Friendship(
 );
 
 CREATE TABLE Photo(
-	photo_ID VARCHAR2(100),
-	album_ID VARCHAR2(100),
+	photo_ID VARCHAR2(100) NOT NULL,
+	album_ID VARCHAR2(100) NOT NULL,
 	photo_caption VARCHAR2(2000),
-	photo_created_time TIMESTAMP(6),
-	photo_modified_time TIMESTAMP(6),
-	photo_linke VARCHAR2(2000),
+	photo_created_time TIMESTAMP(6) NOT NULL,
+	photo_modified_time TIMESTAMP(6) NOT NULL,
+	photo_linke VARCHAR2(2000) NOT NULL,
 	PRIMARY KEY (photo_ID)
 );
 
 CREATE TABLE Album(
-	album_ID VARCHAR2(100),
-	cover_photo_ID VARCHAR2(100),
-	owner_user_ID VARCHAR2(100),
-	album_name VARCHAR2(100),
-	album_created_time TIMESTAMP(6),
-	album_modified_time TIMESTAMP(6),
-	album_link VARCHAR2(2000),
-	album_visibility VARCHAR2(100),
+	album_ID VARCHAR2(100) NOT NULL,
+	cover_photo_ID VARCHAR2(100) NOT NULL,
+	owner_user_ID VARCHAR2(100) NOT NULL,
+	album_name VARCHAR2(100) NOT NULL,
+	album_created_time TIMESTAMP(6) NOT NULL,
+	album_modified_time TIMESTAMP(6) NOT NULL,
+	album_link VARCHAR2(2000) NOT NULL,
+	album_visibility VARCHAR2(100) NOT NULL,
 	PRIMARY Key (album_ID),
-	FOREIGN Key (cover_photo_ID) REFERENCES Photo,
 	FOREIGN Key (owner_user_ID) REFERENCES FB_User
 );
 
-ALTER TABLE Photo ADD CONSTRAINT PhotoAlbumFK FOREIGN KEY (album_ID) REFERENCES Album;
-
+ALTER TABLE Photo ADD CONSTRAINT FK_AlbumIDInPhoto FOREIGN KEY (album_ID) REFERENCES Album INITIALLY DEFERRED DEFERRABLE;
+ALTER TABLE Album ADD CONSTRAINT FK_CoverPhotoID FOREIGN Key (cover_photo_ID) REFERENCES Photo INITIALLY DEFERRED DEFERRABLE;
 CREATE TABLE Tag(
-	photo_ID VARCHAR2(100),
-	user_ID VARCHAR2(100),
-	tag_x_coordinate Number,
-	tag_y_coordinate Number,
-	tag_created_time TIMESTAMP(6),
+	photo_ID VARCHAR2(100) NOT NULL,
+	user_ID VARCHAR2(100) NOT NULL,
+	tag_x_coordinate Number NOT NULL,
+	tag_y_coordinate Number NOT NULL,
+	tag_created_time TIMESTAMP(6) NOT NULL,
 	PRIMARY Key (photo_ID, user_ID),
 	FOREIGN Key (photo_ID) REFERENCES Photo,
 	FOREIGN Key (user_ID) REFERENCES FB_User
